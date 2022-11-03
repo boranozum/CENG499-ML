e5340377db947765ba887811033fad3e0263efe7
AHMET BORAN OZUM <e2380814@ceng.metu.edu.tr>
2022-11-03 15:19:18 +0300

    First commit

---
+++ part3.py
@@ -0,0 +1,28 @@
+import torch
+import torch.nn as nn
+import numpy as np
+import pickle
+
+# we load all the datasets of Part 3
+x_train, y_train = pickle.load(open("data/mnist_train.data", "rb"))
+x_validation, y_validation = pickle.load(open("data/mnist_validation.data", "rb"))
+x_test, y_test = pickle.load(open("data/mnist_test.data", "rb"))
+
+x_train = x_train/255.0
+x_train = x_train.astype(np.float32)
+
+x_test = x_test / 255.0
+x_test = x_test.astype(np.float32)
+
+x_validation = x_validation/255.0
+x_validation = x_validation.astype(np.float32)
+
+# and converting them into Pytorch tensors in order to be able to work with Pytorch
+x_train = torch.from_numpy(x_train)
+y_train = torch.from_numpy(y_train).to(torch.long)
+
+x_validation = torch.from_numpy(x_validation)
+y_validation = torch.from_numpy(y_validation).to(torch.long)
+
+x_test = torch.from_numpy(x_test)
+y_test = torch.from_numpy(y_test).to(torch.long)
