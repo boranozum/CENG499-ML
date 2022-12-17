3ca44a62bf929e5190b0764b97354d7644413471
boranozum <boranozum@gmail.com>
2022-12-17 19:43:45 +0300

    part 2 completed

--- Assignment_3/Part2/svm_dataset1.py
+++ Assignment_3/Part2/svm_dataset1.py
@@ -1,5 +1,44 @@
 import pickle
 import numpy as np
+import matplotlib.pyplot as plt
 
+from sklearn.svm import SVC
+from sklearn.inspection import DecisionBoundaryDisplay
 
 dataset, labels = pickle.load(open("../data/part2_dataset1.data", "rb"))
+
+configurations = [
+    {
+        "kernel": "sigmoid",
+        "C": 1,
+    },
+    {
+        "kernel": "sigmoid",
+        "C": 10,
+    },
+    {
+        "kernel": "rbf",
+        "C": 1,
+    },
+    {
+        "kernel": "rbf",
+        "C": 10,
+    },
+]
+
+classifiers = []
+
+for configuration in configurations:
+    clf = SVC(kernel=configuration["kernel"], C=configuration["C"]).fit(dataset, labels)
+    classifiers.append(clf)
+
+f, ax = plt.subplots(2, 2, figsize=(10, 10))
+for i in range(2):
+    for j in range(2):
+        clf = classifiers[i*2+j]
+        display = DecisionBoundaryDisplay.from_estimator(clf, dataset, ax=ax[i][j], alpha=0.7,
+                                                         response_method="predict", xlabel=labels[0], ylabel=labels[1])
+        display.ax_.scatter(dataset[:, 0], dataset[:, 1], c=labels, edgecolors='k')
+        ax[i][j].set_title(f"Kernel: {clf.kernel}, C: {clf.C}")
+plt.show()
+
--- Assignment_3/Part2/svm_dataset2.py
+++ Assignment_3/Part2/svm_dataset2.py
@@ -1,5 +1,42 @@
 import pickle
 import numpy as np
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import StratifiedKFold, GridSearchCV
+from sklearn.svm import SVC
 
 dataset, labels = pickle.load(open("../data/part2_dataset2.data", "rb"))
+
+scaler = StandardScaler()
+
+skf = StratifiedKFold(n_splits=10, shuffle=True)
+
+processed_dataset = scaler.fit_transform(dataset, labels)
+
+configurations = [
+    {
+        'kernel': ['rbf', 'sigmoid'],
+        'C': [1, 10]
+    }
+]
+
+mean_scores = [None for _ in range(4)]
+
+for i in range(5):
+    clf = SVC()
+    grid = GridSearchCV(clf, configurations, cv=skf, scoring="accuracy")
+    grid.fit(processed_dataset, labels)
+    for j in range(4):
+        if mean_scores[j] is None:
+            result = {"params": grid.cv_results_["params"][j], "mean_test_score": [grid.cv_results_["mean_test_score"][j]]}
+            mean_scores[j] = result
+        else:
+            mean_scores[j]["mean_test_score"].append(grid.cv_results_["mean_test_score"][j])
+
+for i in range(4):
+    # calculate confidence interval
+    mean_value = np.mean(mean_scores[i]["mean_test_score"])
+    std_value = np.std(mean_scores[i]["mean_test_score"])
+    confidence_interval = 1.96 * std_value / np.sqrt(5)
+    print(f"Mean score: {mean_value}, Confidence interval: {confidence_interval}, Parameters: {mean_scores[i]['params']}")
 
