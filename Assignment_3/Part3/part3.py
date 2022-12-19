f5a3fa77e62ad99ef4c8922840f74dfcb3dbfd73
boranozum <boranozum@gmail.com>
2022-12-19 18:44:16 +0300

    part 3 ready for testing

--- Assignment_3/Part3/part3.py
+++ Assignment_3/Part3/part3.py
@@ -134,7 +134,6 @@ for train_index, test_index in outer_cv.split(dataset, labels):
 for model in results:
 
     print(model)
-
     print('hyperparameter_scores')
     for config in results[model]['hyperparameter_scores']:
         # calculate mean and confidence interval
